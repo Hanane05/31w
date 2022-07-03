2a87ff68311441cf465b2bbae4cac37291255caa
Hanane05 <e2195837@cmaisonneuve.qc.ca>
2022-07-03 09:24:30 -0400

    s6-c12- Modifier le h1 dans index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
  * @package mon_31w
  */
 get_header(); ?>
-<h1> Mon premier modèle Wordpress</h1>
+<h1> Mon modèle Wordpress pour Tp1</h1>
 <?php
 if (have_posts()) :
     while (have_posts()) : the_post(); ?>
