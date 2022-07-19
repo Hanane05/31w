c13330143032786ed45af1e4ee24a0ec8f3f439c
Hanane05 <e2195837@cmaisonneuve.qc.ca>
2022-07-18 23:10:03 -0400

    s8-c2 Modifier h1 dans front-page.php et single.php

--- front-page.php
+++ front-page.php
@@ -1,24 +1,24 @@
 <?php
 get_header(); ?>
 <section class="site__main">
-<h1> front-page Mon modèle Wordpress pour TP1</h1>
-<?php
-if (have_posts()) :
-    while (have_posts()) : the_post(); ?>
-        <h2><?php the_title(); ?></h2>
-   
-        <?php $lien = " ... <a class='bouton' href='" 
-                . get_permalink()  
+    <h1> Mon modèle Wordpress pour TP1</h1>
+    <?php
+    if (have_posts()) :
+        while (have_posts()) : the_post(); ?>
+            <h2><?php the_title(); ?></h2>
+
+            <?php $lien = " ... <a class='bouton' href='"
+                . get_permalink()
                 . "'>"
-                . substr(get_the_title(),0,8) 
-                ."</a>"; 
+                . substr(get_the_title(), 0, 8)
+                . "</a>";
 
-?>
+            ?>
 
-<p><?= wp_trim_words(get_the_content(), 20, $lien) ?></p>
+            <p><?= wp_trim_words(get_the_content(), 20, $lien) ?></p>
 
 
-    <?php endwhile; ?>
-<?php endif; ?>    
+        <?php endwhile; ?>
+    <?php endif; ?>
 </section>
 <?php get_footer(); ?>
